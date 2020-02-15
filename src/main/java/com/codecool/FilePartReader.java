5d86a46e25ef7155c69c4a811103bf769621f3b7
Adam <adamwroblewski.1993@gmail.com>
2020-02-15 16:13:10 +0100

    Return given lines from txt file.

--- src/main/java/com/codecool/FilePartReader.java
+++ src/main/java/com/codecool/FilePartReader.java
@@ -8,9 +8,13 @@ import java.nio.file.Paths;
 public class FilePartReader {
 
     private String filePath;
+    int fromLine;
+    int toLine;
 
     public void setup(String filePath, int fromLine, int toLine) {
         this.filePath = filePath;
+        this.fromLine = fromLine;
+        this.toLine = toLine;
 
         if (toLine < fromLine)
             throw new IllegalArgumentException("toLine can't be lowe than fromLine!");
@@ -21,10 +25,18 @@ public class FilePartReader {
 
     public String read() throws IOException {
 
-        return Files.readString(Paths.get(filePath), StandardCharsets.US_ASCII);
+        return Files.readString(Paths.get(filePath), StandardCharsets.UTF_8);
     }
 
     public String readLines() throws IOException {
-        return read();
+        String newline = System.getProperty("line.separator");
+        String[] textLines = read().split(newline);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = fromLine - 1; i < toLine; i++) {
+            stringBuilder.append(textLines[i]).append(newline);
+        }
+
+        return stringBuilder.toString();
     }
 }

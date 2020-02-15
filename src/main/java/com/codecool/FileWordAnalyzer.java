aa9ff872dd40752c2b86155e0ad09522dd6b813e
Adam <adamwroblewski.1993@gmail.com>
2020-02-15 17:07:47 +0100

    Find palindromes in given words.

--- src/main/java/com/codecool/FileWordAnalyzer.java
+++ src/main/java/com/codecool/FileWordAnalyzer.java
@@ -20,7 +20,7 @@ public class FileWordAnalyzer {
         return wordsList;
     }
 
-    public List<String> getWordsContainingSubstring (String subString ) throws IOException {
+    public List<String> getWordsContainingSubstring(String subString) throws IOException {
 
         wordsList = new ArrayList<>(Arrays.asList(filePartReader.readLines().split("\\s")));
 
@@ -30,5 +30,26 @@ public class FileWordAnalyzer {
     }
 
 
-//    public List getStringsWhichPalindromes (){}
+    public List<String> getStringsWhichPalindromes() throws IOException {
+
+        wordsList = new ArrayList<>(Arrays.asList(filePartReader.readLines().split("\\s")));
+        List<String> palindromes = new ArrayList<>();
+
+        Stack<Character> stack1 = new Stack<>();
+        StringBuilder reversedWord = new StringBuilder();
+
+        for (String word : wordsList){
+            reversedWord.setLength(0);
+            for (char letter : word.toCharArray()){
+                stack1.push(letter);
+            }
+            while (stack1.size() > 0){
+                reversedWord.append(stack1.pop());
+            }
+            if (word.equals(reversedWord.toString())){
+                palindromes.add(reversedWord.toString());
+            }
+        }
+        return palindromes;
+    }
 }

31296898670acf189f33bd98c8fea55283858493
Adam <adamwroblewski.1993@gmail.com>
2020-02-16 10:27:58 +0100

    Test methods from FilePartReader class.

---
+++ src/main/test/java/com/codecool/FilePartReaderTest.java
@@ -0,0 +1,51 @@
+package com.codecool;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FilePartReaderTest {
+    private FilePartReader filePartReader = new FilePartReader();
+    private String filePath = "target/classes/txtFiles/module_oop.md";
+    private Integer fromLine = 1;
+    private Integer toLine = 5;
+
+
+    @Test
+    public void testSetupMethodParameters() {
+        filePartReader.setup(filePath, fromLine, toLine);
+
+        assertEquals(String.class, filePath.getClass());
+        assertEquals(Integer.class, fromLine.getClass());
+        assertEquals(Integer.class, toLine.getClass());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetupForThrownExceptionWhenToLineIsSmallerThanFromLine() {
+        filePartReader.setup("target/classes/txtFiles/module_oop.md", 3, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetupForThrownExceptionWhenFromLineIsLowerThanZero() {
+        filePartReader.setup("target/classes/txtFiles/module_oop.md", 0, 13);
+    }
+
+    @Test
+    public void testIfReadMethodReturnsString() throws IOException {
+        filePartReader.setup("target/classes/txtFiles/module_oop.md", 1, 1);
+        assertEquals(String.class, filePartReader.read().getClass());
+    }
+
+    @Test(expected = IOException.class)
+    public void testIfFilePathIsValid() throws IOException {
+        filePartReader.setup("invalidPath.txt", 1, 1);
+        filePartReader.read();
+    }
+
+    @Test()
+    public void testIfReadLinesMethodUsesReadMethod() {
+
+    }
+}

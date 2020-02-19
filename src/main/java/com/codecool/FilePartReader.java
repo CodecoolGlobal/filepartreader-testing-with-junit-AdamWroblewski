44344b79e4a918878a32f49abc21f648ba6f53f1
Adam <adamwroblewski.1993@gmail.com>
2020-02-19 10:34:06 +0100

    Make variable private, fulfillment todo.

--- src/main/java/com/codecool/FilePartReader.java
+++ src/main/java/com/codecool/FilePartReader.java
@@ -8,8 +8,8 @@ import java.nio.file.Paths;
 public class FilePartReader {
 
     private String filePath;
-    Integer fromLine;
-    Integer toLine;
+    private Integer fromLine;
+    private Integer toLine;
 
     public void setup(String filePath, Integer fromLine, Integer toLine) {
         this.filePath = filePath;
